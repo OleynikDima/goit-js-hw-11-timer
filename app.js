57695422f19fc914e3663593dce93b8cab69e18b
OleynikDima <57680550+OleynikDima@users.noreply.github.com>
2020-05-25 20:50:55 +0300

    change setInterval and add consturctor itemTeg

--- app.js
+++ app.js
@@ -1,30 +1,25 @@
 
 class CountdownTimer{
   constructor({selector,targetDate}){
-    this.selector = document.querySelector(`${selector}`);
     this.targetDate = targetDate;
+    this.selector = document.querySelector(`${selector}`);
+    this.days = this.selector.querySelector('span[data-value="days"]');
+    this.hours = this.selector.querySelector('span[data-value="hours"]');
+    this.mins = this.selector.querySelector('span[data-value="mins"]');
+    this.secs = this.selector.querySelector('span[data-value="secs"]');
   }
-  
+
   timer(){
-    return this.timerid = setInterval(() => {
+    return setInterval(() => {
       const timeNow =new Date();
       let diffTime = this.targetDate - timeNow;
-
-      const days = Math.floor(diffTime / (1000 * 60 * 60 * 24));
-      this.selector.querySelector('span[data-value="days"]').textContent = days;
-
-      const hours = Math.floor((diffTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      this.selector.querySelector('span[data-value="hours"]').textContent = hours;
-
-      const mins = Math.floor((diffTime % (1000 * 60 * 60)) / (1000 * 60));
-      this.selector.querySelector('span[data-value="mins"]').textContent = mins;
-
-      const secs = Math.floor((diffTime % (1000 * 60)) / 1000);
-      this.selector.querySelector('span[data-value="secs"]').textContent = secs;
-    }, 1000);
+      this.days.textContent = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+      this.hours.textContent = Math.floor((diffTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      this.mins.textContent = Math.floor((diffTime % (1000 * 60 * 60)) / (1000 * 60));
+      this.secs.textContent = Math.floor((diffTime % (1000 * 60)) / 1000);
+    });
   }
 }
-
 
 const counterDown = new CountdownTimer({
   selector: '#timer-1',
